d6401490b93ce9e256d32485b1066b628f7aafeb
Noluthando05 <104364511+Noluthando05@users.noreply.github.com>
2025-03-04 03:07:52 +0200

    hey

--- obj/Debug/net8.0/SKY Journey.AssemblyInfo.cs
+++ obj/Debug/net8.0/SKY Journey.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SKY Journey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7448c3547e6696bd92b8006d413e49efde01832")]
 [assembly: System.Reflection.AssemblyProductAttribute("SKY Journey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SKY Journey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
